1abdf2352a0ef90251415ba2ac8413cd97d0bdeb
RPS98 <rafa.perez.s98@gmail.com>
2022-12-16 13:41:49 +0100

    Update to new launcher files

--- as2_launch.bash
+++ as2_launch.bash
@@ -8,7 +8,7 @@ fi
 # Arguments
 drone_namespace=$1
 use_sim_time=true
-behavior_type="position" # position or trajectory
+behavior_type="position" # "position" or "trajectory"
 
 source ./utils/launch_tools.bash
 
@@ -16,28 +16,31 @@ new_session $drone_namespace
 
 new_window 'as2_ignition_platform' "ros2 launch as2_ignition_platform ignition_platform_launch.py \
     drone_id:=$drone_namespace \
-    config_file:=simulation_config/default.json \
-    use_sim_time:=$use_sim_time"
+    use_sim_time:=$use_sim_time \
+    config_file:=simulation_config/default.json"
 
 new_window 'as2_controller_manager' "ros2 launch as2_controller_manager controller_manager_launch.py \
     namespace:=$drone_namespace \
+    use_sim_time:=$use_sim_time \
+    cmd_freq:=100.0 \
+    info_freq:=10.0 \
     use_bypass:=true \
-    config:=config/$controller/controller.yaml \
-    use_sim_time:=$use_sim_time"
+    plugin_name:=controller_plugin_speed_controller \
+    plugin_config_file:=config/controller.yaml"
 
 new_window 'as2_state_estimator' "ros2 launch as2_state_estimator state_estimator_launch.py \
     namespace:=$drone_namespace \
-    config:=config/state_estimator.yaml \
-    base_frame:="\/$drone_namespace" \
-    use_sim_time:=$use_sim_time" 
+    use_sim_time:=$use_sim_time \
+    plugin_name:=as2_state_estimator_plugin_ground_truth \
+    plugin_config_file:=config/default_state_estimator.yaml" 
 
 new_window 'as2_platform_behaviors' "ros2 launch as2_platform_behaviors as2_platform_behaviors_launch.py \
     namespace:=$drone_namespace \
-    config_takeoff:=config/$behavior_type/takeoff_behaviour.yaml \
-    config_goto:=config/$behavior_type/goto_behaviour.yaml \
-    config_follow_path:=config/$behavior_type/follow_path_behaviour.yaml \
-    config_land:=config/land_behaviour.yaml \
-    use_sim_time:=$use_sim_time"
+    use_sim_time:=$use_sim_time \
+    follow_path_plugin_name:=follow_path_plugin_$behavior_type \
+    goto_plugin_name:=goto_plugin_$behavior_type \
+    takeoff_plugin_name:=takeoff_plugin_$behavior_type \
+    land_plugin_name:=land_plugin_speed"
 
 if [[ "$behavior_type" == "trajectory" ]]
 then
