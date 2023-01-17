0f4ff9640ff7dd47ad3410001974cf045430e606
pariaspe <pedro.ariasp@upm.es>
2023-01-17 16:38:03 +0100

    removed unnecessary export

--- launch_ignition.bash
+++ launch_ignition.bash
@@ -3,12 +3,6 @@
 config_path="$1"
 config_path=${config_path:="simulation_config/default.json"}
 
-PROJECT_PATH="$(pwd)"
-AS2_MODELS="${PROJECT_PATH}/as2_simulation_assets/as2_ign_gazebo_assets/models"
-AS2_WORLDS="${PROJECT_PATH}/as2_simulation_assets/as2_ign_gazebo_assets/worlds"
-
-export IGN_GAZEBO_RESOURCE_PATH=$IGN_GAZEBO_RESOURCE_PATH:$AS2_MODELS:$AS2_WORLDS
-
 export RUN_ON_START=1
 
 SCRIPT_PATH="${AEROSTACK2_PATH}/as2_simulation_assets/as2_ign_gazebo_assets/scripts"
