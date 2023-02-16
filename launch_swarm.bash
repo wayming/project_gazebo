770753b376997f08d72c05f05e54492e21137650
RPS98 <rafa.perez.s98@gmail.com>
2023-02-16 13:25:13 +0100

    Add keyboard teleop

--- launch_swarm.bash
+++ launch_swarm.bash
@@ -2,12 +2,11 @@
 
 drone_namespaces=('drone_sim_0' 'drone_sim_1' 'drone_sim_2')
 simulation_config="simulation_config/swarm.json"
-launch_keyboard_teleop="false"
 
 # For each drone namespace
 for drone_namespace in "${drone_namespaces[@]}"
 do
-    ./as2_launch.bash "${drone_namespace}" $simulation_config $launch_keyboard_teleop
+    ./as2_launch.bash "${drone_namespace}" $simulation_config
 done
 
 # Attach to the first session
@@ -19,8 +18,3 @@ else
     tmux attach -t $session:0
 fi
 
-source ./utils/launch_tools.bash
-new_session ${drone_namespaces[0]}_keyboard_teleop
-new_window 'keyboard_teleop' "ros2 launch as2_keyboard_teleoperation as2_keyboard_teleoperation_launch.py  \
-        namespace:=drone_sim_0,drone_sim_1,drone_sim_2 \
-        use_sim_time:=true"
--- main_launcher.bash
+++ main_launcher.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-drone_namespaces=('drone_sim_0')
+$drone_namespace=$1
+if [ -z "$drone_namespace" ]; then
+    drone_namespace="drone_sim_0"
+fi
 simulation_config="simulation_config/default.json"
 launch_keyboard_teleop="true"
 
 # Run nodes
-./as2_launch.bash "${drone_namespaces[0]}" $simulation_config $launch_keyboard_teleop
+./as2_launch.bash "${drone_namespace}" $simulation_config $launch_keyboard_teleop
 
-session=${drone_namespace}$(($n - 1))
+session=${drone_namespace}
 
 # if inside a tmux session detach before attaching to the session
 if [ -n "$TMUX" ]; then
